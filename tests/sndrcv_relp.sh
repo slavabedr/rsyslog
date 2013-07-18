afbcb6d445778c5720a5ba15e7e646705d41fafb
Radu Gheorghe <radu.gheorghe@sematext.com>
2013-07-18 13:34:07 +0300

    50K messages instead of 50, to better test reliability

--- tests/sndrcv_relp.sh
+++ tests/sndrcv_relp.sh
@@ -1,7 +1,9 @@
 # This runs sends and receives messages via RELP to port 20514
+# we're sending 50K messages to actually test if messages
+# still arrive even if they are many
 echo ===============================================================================
 echo \[sndrcv_relp.sh\]: testing sending and receiving via udp
 if [ "$EUID" -ne 0 ]; then
     exit 77 # Not root, skip this test
 fi
-source $srcdir/sndrcv_drvr.sh sndrcv_relp 50
+source $srcdir/sndrcv_drvr.sh sndrcv_relp 50000
