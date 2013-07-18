0f8e517f55489484699eae4e695a161a1ea491c0
Radu Gheorghe <radu.gheorghe@sematext.com>
2013-07-18 13:30:09 +0300

    updated in-script documentation

--- tests/sndrcv_relp.sh
+++ tests/sndrcv_relp.sh
@@ -1,10 +1,4 @@
-# This runs sends and receives messages via UDP to the standard
-# ports. Note that with UDP we can always have message loss. While this is
-# less likely in a local environment, we strongly limit the amount of data
-# we send in the hope to not lose any messages. However, failure of this
-# test does not necessarily mean that the code is wrong (but it is very likely!)
-# added 2009-11-11 by Rgerhards
-# This file is part of the rsyslog project, released  under GPLv3
+# This runs sends and receives messages via RELP to port 20514
 echo ===============================================================================
 echo \[sndrcv_relp.sh\]: testing sending and receiving via udp
 if [ "$EUID" -ne 0 ]; then
